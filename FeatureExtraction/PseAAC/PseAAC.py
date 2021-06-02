85abfb3f9a31796645f47a00bddf3215ad7acedd
Quan <quannar178@gmail.com>
2021-06-02 20:12:41 +0700

    Feature extraction:PseAC

--- FeatureExtraction/PseAAC/PseAAC.py
+++ FeatureExtraction/PseAAC/PseAAC.py
@@ -1,5 +1,6 @@
 # amino = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
 #          'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', 'X']
+from typing import Sequence
 import pandas as pd
 import numpy as np
 import readFasta
@@ -7,8 +8,7 @@ import re
 import sys
 import os
 from collections import Counter
-from propy import PyPro
-from propy.GetProteinFromUniprot import GetProteinSequence
+import math
 # pPath = os.path.split(os.path.realpath(__file__))[0]
 # sys.path.append(pPath)
 
@@ -16,36 +16,70 @@ USAGE = """
 USAGE:
 	python AAP.py input.fasta order <output>
 	input.fasta:      the input protein sequence file in fasta format.
-	order:            the out order, select from ['alphabetically', 'polarity', 'sideChainVolume' or userDefined] 
+	order:            the out order, select from ['alphabetically', 'polarity', 'sideChainVolume' or userDefined]
 	output:           the encoding file, default: 'encodings.tsv'
 """
 
 
-def PseAAC(fastas, feed_lambda=10, feed_weight=0.05, **kw):
-    encodings = []
-    #
-    header = ['#']
-    seq = fastas[0][1]
-    DesObject = PyPro.GetProDes(seq)
-    paac = DesObject.GetPAAC(lamda=feed_lambda, weight=feed_weight)
+def Rvalue(aa1, aa2, AADict, Matrix):
+    return sum([(Matrix[i][AADict[aa1]] - Matrix[i][AADict[aa2]]) ** 2 for i in range(len(Matrix))]) / len(Matrix)
 
-    for name in paac:
-        header.append(name)
-    encodings.append(header)
-    print(header)
+
+def PseAAC(fastas, lambdaValue=30, w=0.05, **kw):
+    # Read data
+    with open("PAAC.txt") as f:
+        records = f.readlines()
+    AA = ''.join(records[0].rstrip().split()[1:])
+    AADict = {}
+
+    for i in range(len(AA)):
+        AADict[AA[i]] = i
+
+    AAProperty = []
+    AAPropertyNames = []
+    for i in range(1, len(records)):
+        array = records[i].rstrip().split(
+        ) if records[i].rstrip() != '' else None
+        AAProperty.append([float(j) for j in array[1:]])
+        AAPropertyNames.append(array[0])
+
+    AAProperty1 = []
+    for i in AAProperty:
+        meanI = sum(i) / 20
+        fenmu = math.sqrt(sum([(j-meanI)**2 for j in i])/20)
+        AAProperty1.append([(j-meanI)/fenmu for j in i])
+
     #
-    count = 1
+    encodings = []
+    header = ['#']
+    for aa in AA:
+        header.append('Xc1.' + aa)
+    for n in range(1, lambdaValue + 1):
+        header.append('Xc2.lambda' + str(n))
+    encodings.append(header)
+    encodings = []
+
+    header = ['#']
+    for aa in AA:
+        header.append('Xc1.' + aa)
+    for n in range(1, lambdaValue + 1):
+        header.append('Xc2.lambda' + str(n))
+    encodings.append(header)
+
     for i in fastas:
-        del DesObject
-        name, sequence = i[0], i[1]
+        name, sequence = i[0], re.sub('-', '', i[1])
         code = [name]
-        DesObject = PyPro.GetProDes(sequence)
-        paac = DesObject.GetPAAC(lamda=feed_lambda, weight=feed_weight)
-        for i in paac:
-            code.append(paac[i])
+        theta = []
+        for n in range(1, lambdaValue + 1):
+            theta.append(
+                sum([Rvalue(sequence[j], sequence[j + n], AADict, AAProperty1) for j in range(len(sequence) - n)]) / (
+                    len(sequence) - n))
+            myDict = {}
+        for aa in AA:
+            myDict[aa] = sequence.count(aa)
+        code = code + [myDict[aa] / (1 + w * sum(theta)) for aa in AA]
+        code = code + [(w * j) / (1 + w * sum(theta)) for j in theta]
         encodings.append(code)
-        print(count)
-        count += 1
     return encodings, header
 
 
@@ -53,7 +87,7 @@ kw = {'path': r"H_train.txt", 'order': 'ACDEFGHIKLMNPQRSTVWY'}
 
 
 if __name__ == "__main__":
-    fastas1 = readFasta.readFasta(r"dev.fasta")
+    fastas1 = readFasta.readFasta(r"data.fasta")
     result, header = PseAAC(fastas1, **kw)
     data1 = np.matrix(result[1:])[:, 1:]
     data2 = np.matrix(result[1:])
